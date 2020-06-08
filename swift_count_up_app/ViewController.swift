80e7ea6a1d62be167a519b9059ab8cebe5ad5c91
Haya <max9173627@gmail.com>
2020-06-08 10:03:34 +0900

    [add]count_up機能の実装

--- swift_count_up_app/ViewController.swift
+++ swift_count_up_app/ViewController.swift
@@ -10,26 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet weak var countLabel: UILabel!
     
-    
+    var count = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        countLabel.text = String(count)
     }
 
 
     @IBAction func plus(_ sender: Any) {
         
+        count = count + 1
+        countLabel.text = String(count)
+
+        if count >= 10 {
+            changeCountColor()
+        }
         
     }
     
     
     @IBAction func minus(_ sender: Any) {
         
+        count = count - 1
+        countLabel.text = String(count)
         
+        if count <= 0 {
+            resetCountColor()
+        }
+        
+    }
+    
+    func changeCountColor(){
+        countLabel.textColor = .green
+    }
+    
+    func resetCountColor(){
+        countLabel.textColor = .white
     }
     
     
